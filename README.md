9022713480c3550bd420da40e037afc374bd56b1
Amanda Nelson <amandaln4311@gmail.com>
2020-12-21 21:38:13 -0500

    added buttons, got answers array to show

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Motorcycle Quiz
 ## Description
-
+For this quiz timer will start to count down after you click the start button. Answer the questions to the bext of your knowledge. For every wrong answer the timer will take off 10 seconds. Once you have finished the quiz your score is equal to the time left. Add your initials to the highscore board and replay!
 
 *https://amandaln.github.io/code_quiz/
 
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,20 +1,64 @@
 // start button and store
 var startBtn = document.getElementById("start");
-var questionsElement = document.getElementById("questions")
+var questionsElement = document.getElementById("questions");
 var startScreen = document.querySelector("#start-screen");
 var timeEll = document.getElementById("time");
-var mainEl = document.querySelector("#end-screen")
+var mainEl = document.querySelector("#end-screen");
+var questionsChoices = document.querySelector("#choices");
+var questionTitle = document.querySelector("#questions-title");
+var questionsIndex = -1;
+var currentQuestion;
 // event listener
 startBtn.onclick = startQuiz;
 
 // funtion to start quiz
 function startQuiz() {
     
-    startScreen.setAttribute("class", "hide")
+    startScreen.setAttribute("class", "hide");
+   
    
 // unhide the questions
     questionsElement.removeAttribute("class");
+    console.log(questions[0].title)
+    console.log(questions[0].choices)
     timeEl();
+    getCurrentQuestion();
+   // getCurrentChoices();
+}
+
+function getCurrentQuestion() {
+    questionsIndex++
+//print the title of the question
+    questionTitle.textContent = questions[questionsIndex].title;
+
+    currentQuestions = questions[questionsIndex].choices;
+console.log(currentQuestion);
+
+
+//var currentChoices = currentQuestion[questionsIndex].choices;
+
+// for loop 
+
+for (let i = 0; i < currentQuestions.length; i++) {
+    var choices2 = document.createElement("button");
+    choices2.textContent = currentQuestions[i];
+    answerBtn = questionsChoices.appendChild(choices2).setAttribute("class", "button");
+
+    }
+
+    if (answer === event.target.textContent) {   
+        pElement.innerHTML = "YES!";
+        setTimeout(hideFeedback,1225);
+        showFeedback();   
+        
+    } else {
+        pElement.innerHTML = "BOOOOO. NO!";
+        setTimeout(hideFeedback,1225);
+        secondsLeft = secondsLeft - 10;
+        showFeedback();
+    }    
+
+
 }
 
 function timeEl() {
